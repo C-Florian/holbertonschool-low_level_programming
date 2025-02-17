c9c0cf06222c8c5ce8678a68eb6e0f516f050651
C-Florian <florianchereau@hotmail.fr>
2025-02-17 15:05:57 +0100

    Script that write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  // Inclusion de la bibliothèque standard pour utiliser printf
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description : Ce programme affiche une phrase en utilisant printf.
+ * Il ne doit pas utiliser la fonction puts et doit retourner 0 à la fin.
+ *
+ * Return: 0 si l'exécution est réussie
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);  // Indique que le programme s'est terminé avec succès
+}
