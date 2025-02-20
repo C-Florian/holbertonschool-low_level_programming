7e7976f4cba62d8e8687734e5544862ba6f52290
C-Florian <florianchereau@hotmail.fr>
2025-02-20 11:07:10 +0100

    header file main.h to declare _putchar function

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H  // Vérifie si MAIN_H n'est pas déjà défini
+#define MAIN_H  // Définit MAIN_H pour éviter les inclusions multiples
+
+int _putchar(char c); // Déclaration de la fonction _putchar
+
+#endif /* MAIN_H */  // Fin de la condition préprocesseur
+
