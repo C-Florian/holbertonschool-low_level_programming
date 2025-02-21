10cebe0a6f28b97d8ee3a5a30dbc3a430ba16cd7
C-Florian <florianchereau@hotmail.fr>
2025-02-21 10:58:22 +0100

    examples you can use to test your functions

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet(); /* Appel de la fonction */
+    return (0);
+}
+
