20cdbeeb42745d9c9209ac89585aa2aff585faae
C-Florian <florianchereau@hotmail.fr>
2025-02-21 17:01:41 +0100

    function that computes the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Calcule la valeur absolue d'un entier.
+ * @n: Le nombre dont on veut la valeur absolue.
+ *
+ * Return: La valeur absolue de n.
+ */
+int _abs(int n)
+{
+    if (n < 0)
+        return (-n);  
+    else
+        return (n);   
+}
+
