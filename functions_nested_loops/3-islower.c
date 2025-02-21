e7df4cda729b23aa3cbe180eedd2dd71ffc5766b
C-Florian <florianchereau@hotmail.fr>
2025-02-21 15:46:56 +0100

    function that checks for lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Vérifie si un caractère est une lettre minuscule
+ * @c: Le caractère à vérifier
+ *
+ * Return: 1 si c est une lettre minuscule, 0 sinon
+ */
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z') // Vérifie si c est entre 'a' et 'z'
+        return (1);
+    else
+        return (0);
+}
+
