1dd15269523675eda3396e9743b7c2d550a12176
C-Florian <florianchereau@hotmail.fr>
2025-03-14 16:52:32 +0100

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,28 +3,6 @@
 #include <string.h>
 
 /**
- * _strdup - Duplique une chaîne de caractères
- * @str: Chaîne à dupliquer
- *
- * Return: Pointeur vers la nouvelle chaîne ou NULL si échec.
- */
-char *_strdup(char *str)
-{
-	char *dup;
-	size_t len;
-
-	if (str == NULL)
-		return (NULL);
-
-	len = strlen(str) + 1;
-	dup = malloc(len);
-	if (dup == NULL)
-		return (NULL);
-
-	return (memcpy(dup, str, len));
-}
-
-/**
  * new_dog - Crée une nouvelle structure dog_t
  * @name: Nom du chien
  * @age: Âge du chien
