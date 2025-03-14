f3b8acffa8c3967e0e7d99768733d02754fae7de
C-Florian <florianchereau@hotmail.fr>
2025-03-14 16:57:44 +0100

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,39 +1,52 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog - Crée une nouvelle structure dog_t
- * @name: Nom du chien
- * @age: Âge du chien
- * @owner: Propriétaire du chien
+ * new_dog - Crée un nouveau chien avec une copie de name et owner.
+ * @name: Nom du chien (chaîne de caractères)
+ * @age: Âge du chien (float)
+ * @owner: Propriétaire du chien (chaîne de caractères)
  *
- * Return: Pointeur vers la nouvelle structure ou NULL si échec.
+ * Return: Pointeur vers le nouveau chien, ou NULL en cas d'échec.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *ptr_dog;
+	int dogname, dogowner, i;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	if (!name || !owner)
 		return (NULL);
 
-	dog->name = _strdup(name);
-	if (dog->name == NULL)
+	ptr_dog = malloc(sizeof(dog_t));
+	if (!ptr_dog)
+		return (NULL);
+
+	for (dogname = 0; name[dogname]; dogname++)
+		;
+	for (dogowner = 0; owner[dogowner]; dogowner++)
+		;
+
+	ptr_dog->name = malloc(dogname + 1);
+	ptr_dog->owner = malloc(dogowner + 1);
+
+	if (!ptr_dog->name || !ptr_dog->owner)
 	{
-		free(dog);
+		free(ptr_dog->name);
+		free(ptr_dog->owner);
+		free(ptr_dog);
 		return (NULL);
 	}
 
-	dog->owner = _strdup(owner);
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
+	for (i = 0; i <= dogname; i++) /* Copie name */
+		ptr_dog->name[i] = name[i];
 
-	dog->age = age;
+	ptr_dog->age = age;
 
-	return (dog);
+	for (i = 0; i <= dogowner; i++) /* Copie owner */
+		ptr_dog->owner[i] = owner[i];
+
+	return (ptr_dog);
 }
