9fe76d380b6d3c0d2d3b4e63ec5befccb9695ed7
C-Florian <florianchereau@hotmail.fr>
2025-04-02 18:50:06 +0200

    chore: Ajout du fichier lists.h avec la structure et les prototypes des fonctions

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,23 +1,31 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - Structure d'une liste chaînée simplement liée
+ * @str: Chaîne de caractères (string) allouée dynamiquement
+ * @len: Longueur de la chaîne de caractères
+ * @next: Pointeur vers le prochain nœud de la liste
  *
- * Description: singly linked list node structure
+ * Description: Structure représentant une liste simplement chaînée
+ * utilisée dans le projet de listes chaînées en C.
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
+/* Prototypes des fonctions */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
