478025b9c99442aad7a5ea4ac6cd5c88db34c120
C-Florian <florianchereau@hotmail.fr>
2025-04-03 14:14:37 +0200

    Ajout de la fonction dlistint_len pour compter le nombre d'éléments dans une liste doubly linked list

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Renvoie le nombre d'éléments dans une liste doubly chaînée
+ * dlistint_len - Compte les éléments d'une liste doubly chaînée
  * @h: Pointeur vers la tête de la liste
  *
- * Return: Le nombre d'éléments de la liste
+ * Return: Nombre d'éléments dans la liste
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		h = h->next;
 		count++;
